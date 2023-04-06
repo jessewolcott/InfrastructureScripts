fba60f2eab4aca3cad1fd5b14e1101e6a76327e9
Jesse <jesse.wolcott@mac.com>
2023-04-06 11:07:54 -0400

    Create DHCPReservationReport.ps1
    Quick report to dump all DHCP reservations in the domain

---
+++ DHCPReservationReport.ps1
@@ -0,0 +1,17 @@
+$DomainDHCP = 'Yes'
+
+if ($DomainDHCP -eq 'Yes'){
+    $DomainName = (Get-ADDomain).forest
+    $DHCPServers = (Get-DHCPServerinDC).DNSName
+    }
+
+$Results = foreach ($DHCPServer in $DHCPServers){
+            if (Test-Connection -ComputerName $DHCPServer -count 1 -Quiet){
+                $DHCPServerInfo = Get-DHCPServerv4Scope -ComputerName $DHCPServer
+                foreach ($Scope in ($DHCPServerInfo.ScopeId)){
+                    Get-DhcpServerv4Reservation -ComputerName $DHCPServer -ScopeId $Scope
+                }
+            }
+        }
+$Results | Out-GridView -Title "DHCP Reservations-$DomainName" -ErrorAction SilentlyContinue
+  
